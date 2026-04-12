581d1885a03285684db49a291686aad7fadd5a0b
limit-coding <2024210195@bupt.cn>
2026-04-12 20:14:08 +0800

    solve dot

--- src/dot.s
+++ src/dot.s
@@ -20,25 +20,39 @@
 dot:
 
     # Prologue
-
-
+    li t0,1
+    blt a2,t0,error_36
+    blt a3,t0,error_37
+    blt a4,t0,error_37
+    
+    li t0,0
+    li t1,0
+    slli t2,a3,2
+    slli t3,a4,2
 loop_start:
-
-
-
-
-
-
-
-
-
-
-
+    bge t1,a2,loop_end
+    lw t4,0(a0)
+    lw t5,0(a1)
+    mul t6 t4 t5
+    add t0 t0 t6
+    add a0 a0 t2
+    add a1,a1,t3
+    addi t1,t1,1
+    j loop_start
 
 loop_end:
-
+    mv a0,t0
 
     # Epilogue
 
 
     jr ra
+    
+
+error_36:
+    li a0,36
+    j exit
+
+error_37:
+    li a0,37
+    j exit
