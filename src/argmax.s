e3225b73bf491cbd4643d8650a8d076b7b1c57cf
limit-coding <2024210195@bupt.cn>
2026-04-12 15:00:18 +0800

    solve relu.s argmax.s

--- src/argmax.s
+++ src/argmax.s
@@ -16,15 +16,33 @@
 # =================================================================
 argmax:
     # Prologue
+    li t0,1
+    bge a1,t0,start_logic
+    addi a0,x0,36
+    j exit
 
-
+start_logic:
+    
+    li t0 0
+    lw t1 0(a0)
+    li t2 0
+    li t3 0
 loop_start:
-
-
+    
+    beq t0,a1,loop_end
+    lw t3,0(a0)
+    bge t1 t3 jump
+    mv t1 t3
+    mv t2 t0
+    
+jump:
+    addi a0 a0 4
+    addi t0 t0 1
+    j loop_start
 loop_continue:
 
 
 loop_end:
     # Epilogue
-
+    mv a0,t2
     jr ra
--- src/relu.s
+++ src/relu.s
@@ -14,17 +14,23 @@
 # ==============================================================================
 relu:
     # Prologue
+    li t0 1
+    bge a1,t0,start_logic
+    li a0 36
+    j exit
 
-
+start_logic:
+    li t0,0
 loop_start:
-
-
-
-
-
-
-
-
+    beq t0,a1,loop_end
+    lw t2,0(a0)
+    bge t2,x0,skip_zero
+    li t2,0
+skip_zero:
+    sw t2,0(a0)
+    addi a0,a0,4
+    addi t0,t0,1
+    j loop_start
 loop_continue:
 
 
