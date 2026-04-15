3b5710ebe9871dd986c51d8bd38087f152daff7f
limit-coding <2024210195@bupt.cn>
2026-04-15 22:38:34 +0800

    solve task7

--- src/read_matrix.s
+++ src/read_matrix.s
@@ -27,16 +27,91 @@
 read_matrix:
 
     # Prologue
+    #ra s0 s1 s2 s3 s4
+    addi sp,sp,-32
+    sw ra,0(sp)
+    sw s0,4(sp)
+    sw s1,8(sp)
+    sw s2,12(sp)
+    sw s3,16(sp)
+    sw s4,20(sp)
+    
+    mv s0,a0 #s0=filename
+    mv s1,a1 #s1=row1_ptr
+    mv s2,a2 #s2=cols_ptr
+    
+    #fopen
+    mv a0,s0
+    li a1,0 #only read
+    jal fopen
+    li t0 -1
+    beq a0,t0,error_27
+    mv s0,a0
 
+    #fread
+    mv a0,s0
+    mv a1,s1 #rows
+    li a2,4
+    jal fread
+    li t0 4
+    bne a0 t0 error_29
+    
+    mv a0,s0 
+    mv a1,s2 #columns
+    li a2,4
+    jal fread
+    li t0 4
+    bne a0 t0 error_29
 
+    #malloc
+    lw t0,0(s1)
+    lw t1 0(s2)
+    mul t2,t0,t1
+    slli t2,t2,2
+    mv s4,t2
 
+    mv a0,s4
+    jal malloc
+    beq a0,x0,error_26
+    mv s3,a0
 
+    #read_matrix
+    mv a0,s0
+    mv a1,s3
+    mv a2,s4
+    jal fread
+    bne a0,s4,error_29
 
-
-
-
+    #close the file
+    mv a0,s0
+    jal fclose
+    li t0,-1
+    beq a0,t0,error_28
 
     # Epilogue
+    mv a0,s3
+    lw ra,0(sp)
+    lw s0,4(sp)
+    lw s1,8(sp)
+    lw s2,12(sp)
+    lw s3,16(sp)
+    lw s4,20(sp)
+    addi sp,sp,32
+    ret
 
+
+error_26:
+    li a0,26
+    j exit
+
+error_27:
+    li a0,27
+    j exit
+error_28:
+    li a0,28
+    j exit
+error_29:
+    li a0,29
+    j exit
 
     jr ra
